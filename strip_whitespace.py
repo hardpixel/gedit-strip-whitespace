3a95d36091c4e74534ff440efe5c91e50ec472dd
jonian <jonian@hardpixel.eu>
2016-08-02 14:54:01 +0300

    rewrite class

--- strip_whitespace.py
+++ strip_whitespace.py
@@ -1,137 +1,70 @@
-import gi
+import gi, re
 
-gi.require_version('Peas', '1.0')
+gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '3.0')
 
 from gi.repository import GObject, Gdk, Gtk, Gedit
 
-class StripWhitespaceViewActivatable(GObject.Object, Gedit.ViewActivatable):
 
-	view = GObject.Property(type=Gedit.View)
+class StripWhitespaceWindowActivatable(GObject.Object, Gedit.WindowActivatable):
+
+	window = GObject.property(type=Gedit.Window)
 
 	def __init__(self):
 		GObject.Object.__init__(self)
 
 	def do_activate(self):
-		self._buffer = self.view.get_buffer()
-		self._completion = self.view.get_completion()
-		self._no_text = True
-
-		self._view_signals = [
-			self.view.connect('notify::buffer', self.on_notify_buffer),
-			self.view.connect('event-after', self.on_key_press_event)
-		]
-
-		self._completion_signals = [
-			self._completion.connect('activate-proposal', self.on_activate_proposal)
-		]
-
-		self._connect_buffer()
-
-	def _connect_buffer(self):
-		self._buffer_signals = [
-		]
-
-	def _disconnect(self, obj, signals):
-		if obj:
-			for sid in signals:
-				obj.disconnect(sid)
-
-		return []
-
-	def _disconnect_buffer(self):
-		self._buffer_signals = self._disconnect(self._buffer, self._buffer_signals)
-		self._buffer_signals = []
-
-	def _disconnect_view(self):
-		self._disconnect(self.view, self._view_signals)
-		self._view_signals = []
-
-	def _disconnect_completion(self):
-		self._disconnect(self._completion, self._completion_signals)
-		self._completion_signals = []
+		self.handler_id = self.window.connect('tab-added', self.on_tab_added)
 
 	def do_deactivate(self):
-		self._disconnect_buffer()
-		self._disconnect_completion()
-		self._disconnect_view()
+		self.window.disconnect(self.handler_id)
 
-		self._buffer = None
-		self._completion = None
+	def on_tab_added(self, window, tab, data=None):
+		self.doc = tab.get_document()
+		self.doc.connect('save', self.on_document_save)
 
-	def do_update_state(self):
-		pass
+	def on_document_save(self, document, data=None):
+		self.strip_trailing_spaces()
+		self.strip_eof_newlines()
 
-	def on_notify_buffer(self, view, gspec):
-		self._disconnect_buffer()
+	def strip_trailing_spaces(self):
+		text = self.doc.get_text(self.doc.get_start_iter(), self.doc.get_end_iter(), False)
+		compiledpattern = re.compile('.*?([ \t]+)$', flags=re.MULTILINE)
 
-		self._buffer = view.get_buffer()
-		self._connect_buffer()
+		start_iter = self.doc.get_start_iter()
+		end_iter   = self.doc.get_start_iter()
 
-	def on_key_press_event(self, view, event):
-		if event.type != Gdk.EventType.KEY_PRESS:
-			return
+		line_no        = 0 # Last matched line no
+		last_match_pos = 0 # Last matched position in the string
 
-		if not self._no_text:
-			self._no_text = True
-			return
+		for match in re.finditer(compiledpattern, text):
+			# Count the newlines since the last match
+			line_no += text.count('\n', last_match_pos, match.start())
 
-		state = event.state
-		key = event.keyval
+			# Work out the offsets within the line
+			whitespace_start = match.start(1) - match.start()
+			whitespace_end   = match.end(1) - match.start()
 
-		if (key == Gdk.KEY_Return or key == Gdk.KEY_KP_Enter) and not (state & Gdk.ModifierType.SHIFT_MASK):
-			piter = self._buffer.get_iter_at_mark(self._buffer.get_insert())
+			# Update the iterators and do the deletion
+			start_iter.set_line(line_no)
+			start_iter.set_line_offset(whitespace_start)
 
-			if not piter.backward_line():
-				return
+			end_iter.set_line(line_no)
+			end_iter.set_line_offset(whitespace_end)
 
-			end = piter.copy()
-			end.forward_to_line_end()
+			self.doc.delete(start_iter, end_iter)
 
-			extraindent = 0
+			# Update the last match position
+			last_match_pos = match.end()
 
-			endings = {']': '[', ')': '('}
-			starts = ('[', '(')
-			ignore = []
+	def strip_eof_newlines(self):
+		itr = self.doc.get_end_iter()
 
-			while not end.starts_line():
-				if not end.backward_char():
+		if itr.starts_line():
+			while itr.backward_char():
+				if not itr.ends_line():
+					itr.forward_to_line_end()
 					break
 
-				ch = end.get_char()
-
-				if ch in endings:
-					ignore.append(endings[ch])
-					continue
-
-				if ch in starts:
-					if ch in ignore:
-						ignore.remove(ch)
-						continue
-
-					start = end.copy()
-					start.set_line_offset(0)
-
-					extraindent = len(start.get_text(end).lstrip()) + 1
-					break
-
-			end = piter.copy()
-			stripit = True
-
-			while not end.ends_line():
-				if not end.get_char().isspace():
-					stripit = False
-					break
-
-				if not end.forward_char():
-					stripit = False
-					break
-
-			if stripit:
-				self._buffer.delete(piter, end)
-
-			if extraindent > 0:
-				piter = self._buffer.get_iter_at_mark(self._buffer.get_insert())
-				self._buffer.insert(piter, ' ' * extraindent)
-
-	def on_activate_proposal(self, completion):
-		self._no_text = False
+			self.doc.delete(itr, self.doc.get_end_iter())
+			self.doc.insert(self.doc.get_end_iter(), "\n")
